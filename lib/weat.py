ce3e81a42cdaf07b9a5335aa2dcecf907b80d8b9
Adi Maini <adimaini@vt.edu>
2020-11-20 15:20:47 -0500

    changes with p value and permutation implementation

--- lib/weat.py
+++ lib/weat.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 class Weat:
 
@@ -9,7 +10,6 @@ class Weat:
         Parameters: 
             tar (np.array): target variable vector
             att (np.array): attribute variable vector
-
         Returns: 
             score (float): cosine similarity score of the two vectors
         '''
@@ -20,14 +20,12 @@ class Weat:
     def mean_cos_similarity(self, tar, att): 
         '''
         Calculates the mean of the cosine similarity between the target and the range of attributes
-
         Parameters: 
             tar (np.array): target variable vector
             att (np.array): attrbute variable matrix for the an attribute
         
         Returns: 
             mean_cos (float): float type value of the mean cosine similarity between the target and the range of attributes
-
         Example: 
             tar (np.array): vector of word embeddings for "Programmer" 
             att (np.array): matrix of word embeddings for males (man, husband, male, etc)
@@ -39,7 +37,6 @@ class Weat:
     def association(self, tar, att1, att2):
         '''
         Calculates the mean association between a single target and all of the attributes
-
         Parameters: 
             tar (np.array): target variable vector
             att1 (np.array): attrbute variable matrix for the first attribute
@@ -47,7 +44,6 @@ class Weat:
         
         Returns: 
             association (float): float type value of the association between the target (single) vs the attributes
-
         Example: 
             tar (np.array): vector of word embeddings for "Programmer" 
             att1 (np.array): matrix of word embeddings for males (man, husband, male, etc)
@@ -56,11 +52,50 @@ class Weat:
         association = self.mean_cos_similarity(tar, att1) - self.mean_cos_similarity(tar, att2)
         return association
 
+    def differential_association(self, t1, t2, att1, att2):
+        '''
+        xyz
+        '''
+        diff_association = np.sum([self.association(tar1, att1, att2) for tar1 in t1]) - \
+                        np.sum([self.association(tar2, att1, att2) for tar2 in t2])
+        return diff_association
+
+
+    def p_value(self, t1, t2, att1, att2): 
+        '''
+        xyz
+        '''
+        diff_association = self.differential_association(t1, t2, att1, att2)
+        target_words = np.concatenate([t1, t2])
+        np.random.shuffle(target_words)
+
+        # check if join of t1 and t2 have even number of elements, if not, remove last element
+        if target_words.shape[0] % 2 != 0:
+            target_words = target_words[:-1]
+
+        # partition_differentiation = []
+        # for permutation in itertools.islice(itertools.permutations(target_words), 0, 10000):
+        #     tar1_words = np.array(permutation[:len(target_words) // 2])
+        #     tar2_words = np.array(permutation[len(target_words) // 2:])
+        #     partition_differentiation.append(
+        #         self.differential_association_sing(tar1_words, tar2_words, att1, att2)
+        #         )
+
+        partition_differentiation = []
+        for i in range(10000):
+            seq = np.random.permutation(target_words)
+            tar1_words = seq[:len(target_words) // 2]
+            tar2_words = seq[len(target_words) // 2:]
+            partition_differentiation.append(
+                self.differential_association(tar1_words, tar2_words, att1, att2)
+                )
+        
+        p_val = np.sum(np.array(partition_differentiation) > diff_association) / len(partition_differentiation)
+        return p_val, diff_association, partition_differentiation
 
     def effect_size(self, t1, t2, att1, att2):
         '''
         Calculates the effect size (d) between the two target variables and the attributes
-
         Parameters: 
             t1 (np.array): first target variable matrix
             t2 (np.array): second target variable matrix
