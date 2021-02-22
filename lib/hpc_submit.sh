392fe4c3d3dafa7b320626dfa789f53f1b0550ef
Adi Maini <adimaini@vt.edu>
2021-02-22 17:51:39 -0500

    deepspeech haitian creole alphabet, bash script changes'

--- lib/hpc_submit.sh
+++ lib/hpc_submit.sh
@@ -2,19 +2,13 @@
 #SBATCH --time 10:00
 #SBATCH -o testing%j.out
 #SBATCH -e testing%j.err
-#SBATCH -p small-gpu -N 2
+#SBATCH -p large-gpu -N 1
 #SBATCH --mail-user=adimaini@gwu.edu 
 #SBATCH --mail-type=ALL
 
 module load python3/3.7.2
 source "/lustre/groups/caliskangrp/adimaini/deepspeech/deepspeech-env/bin/activate"
 cd /lustre/groups/caliskangrp/adimaini/deepspeech/DeepSpeech
-
-set -xe
-
-# Force only one visible device because we have a single-sample dataset
-# and when trying to run on multiple devices (like GPUs), this will break
-export CUDA_VISIBLE_DEVICES=0
 
 python -u DeepSpeech.py --noshow_progressbar \
   --train_files data/cmu/train.csv \
@@ -26,7 +20,7 @@ python -u DeepSpeech.py --noshow_progressbar \
   --n_hidden 100 \
   --epochs 200 \
   --learning_rate 0.001 --dropout_rate 0.05 \
-  --checkpoint_dir '/SEAS/home/adimaini/deepspeech/outputs' \
+  --checkpoint_dir '/SEAS/home/adimaini/deepspeech/outputs' 
   # --audio_sample_rate 1
   # --scorer_path '/home/adimaini/deepspeech/outputs/pruned_lm.scorer' \
 
